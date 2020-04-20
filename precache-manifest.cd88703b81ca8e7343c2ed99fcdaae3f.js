db20d925356b970549b52ff9940f5f163f827e69
Reggie Cheriscat <reginal.cheriscat@gmail.com>
2020-04-20 15:22:30 -0400

    Updates

---
+++ precache-manifest.cd88703b81ca8e7343c2ed99fcdaae3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a165bb26c82ec6d6750852c2ab7893cb",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "8eec85b72a28e1c0c97f",
+    "url": "/expense-tracker-react/static/css/main.bccd0344.chunk.css"
+  },
+  {
+    "revision": "f4ddcfe215580c67b265",
+    "url": "/expense-tracker-react/static/js/2.6dc0619a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.6dc0619a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eec85b72a28e1c0c97f",
+    "url": "/expense-tracker-react/static/js/main.8d795391.chunk.js"
+  },
+  {
+    "revision": "738b4ae4340c4883cee1",
+    "url": "/expense-tracker-react/static/js/runtime-main.1a506040.js"
+  }
+]);
